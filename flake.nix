ea1dbe2baeaae8a5c109ace63985aba5d0304c92
Carter Ison <carter@isons.org>
2023-09-04 14:52:10 -0700

    Implement basic NixOS module functionality

--- flake.nix
+++ flake.nix
@@ -3,7 +3,17 @@
 description = "Pin your NixOS flake inputs to the system flake registry";
 
 outputs = { self }: {
-  nixosModules.default = {
+  nixosModules.default = { config, lib, ... }: with lib; {
+    options.pins = {
+      inputs = mkOption {
+        type = types.attrsOf types.anything;
+        description = mdDoc "The set of inputs to pin";
+      };
+    };
+
+    config.nix.registry = let
+      mkPin = name: value: { ${name}.flake = value; };
+    in concatMapAttrs mkPin config.pins.inputs;
   };
 };
 
